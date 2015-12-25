1724da12b88029777f33b93e27c253e97bd4f8a2
Vladimir Zhukov <voldyjeengle@gmail.com>
2015-12-25 20:27:33 +0100

    scss and css framework configuration

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: ['bower_components/material-design-lite/src']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +21,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import(app.bowerDirectory + '/material-design-lite/material.css');
 
   return app.toTree();
 };
